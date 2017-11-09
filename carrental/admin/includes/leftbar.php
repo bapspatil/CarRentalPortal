06f687e5a4764dcb656566e5594ac0ef8143aa28
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-09 13:18:31 +0530

    Made some changes in Admin Dashboard

--- carrental/admin/includes/leftbar.php
+++ carrental/admin/includes/leftbar.php
@@ -1,29 +1,25 @@
 	<nav class="ts-sidebar">
 			<ul class="ts-sidebar-menu">
 			
-				<li class="ts-label">Main</li>
-				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<!-- <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li> -->
 			
 <li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
 <ul>
-<li><a href="create-brand.php">Create Brand</a></li>
-<li><a href="manage-brands.php">Manage Brands</a></li>
+<li><a href="create-brand.php">List A Brand</a></li>
+<li><a href="manage-brands.php">Manage Existing Brands</a></li>
 </ul>
 </li>
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
+<li><a href="#"><i class="fa fa-sitemap"></i> Cars</a>
 					<ul>
-						<li><a href="post-avehical.php">Post a Vehicle</a></li>
-						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+						<li><a href="post-avehical.php">List A Car</a></li>
+						<li><a href="manage-vehicles.php">Manage Existing Cars</a></li>
 					</ul>
 				</li>
-				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Bookings</a></li>
 
-				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Manage Conatctus Query</a></li>
-				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
-			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
-			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Reviews</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Registered Users</a></li>
 
 			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
 
