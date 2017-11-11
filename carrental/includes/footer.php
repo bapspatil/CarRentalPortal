5a87cf5d8a74a46ab9c34cc679213ed9bfaf4f0c
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-11 19:15:39 +0530

    Final footer

--- carrental/includes/footer.php
+++ carrental/includes/footer.php
@@ -31,35 +31,6 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 ?>
 
 <footer>
-  <div class="footer-top">
-    <div class="container">
-      <div class="row">
-      
-        <div class="col-md-6">
-          <h6>About Us</h6>
-          <ul>
-
-        
-          <li><a href="page.php?type=aboutus">About Us</a></li>
-            <li><a href="page.php?type=faqs">FAQs</a></li>
-               <li><a href="admin/">Admin Login</a></li>
-          </ul>
-        </div>
-  
-        <div class="col-md-3 col-sm-6">
-          <h6>Subscribe Newsletter</h6>
-          <div class="newsletter-form">
-            <form method="post">
-              <div class="form-group">
-                <input type="email" name="subscriberemail" class="form-control newsletter-input" required placeholder="Enter Email Address" />
-              </div>
-              <button type="submit" name="emailsubscibe" class="btn btn-block">Subscribe <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
   <div class="footer-bottom">
     <div class="container">
       <div class="row">
