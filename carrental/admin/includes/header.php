bdd29ae9b1fb6e45360e96d8666fc379a3b0e40a
Bapusaheb Patil <bapoo1998@gmail.com>
2017-11-11 19:52:49 +0530

    Removed Change Password for admin

--- carrental/admin/includes/header.php
+++ carrental/admin/includes/header.php
@@ -1,14 +1,9 @@
 <div class="brand clearfix">
-	<a href="reg-users.php" style="font-size: 20px;">&nbsp;&nbsp;Car Rental Portal | Admin Area</a>  
+	<a href="reg-users.php" style="font-size: 20px;">&nbsp;&nbsp;&nbsp;Admin Area</a>  
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		<ul class="ts-profile-nav">
-			
 			<li class="ts-account">
-				<a href="#"> Account <i class="fa fa-angle-down hidden-side"></i></a>
-				<ul>
-					<li><a href="change-password.php">Change Password</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
+				<a href="logout.php"> Logout </a>
 			</li>
 		</ul>
 	</div>
